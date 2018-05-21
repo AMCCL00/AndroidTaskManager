d12fe0cd8dd6f68dd94a8c9dc20f3c47daa44fb3
Andrew McClary <andrew.mccalry00@gmail.com>
2018-05-21 15:11:11 -0400

    added On clicks\ for adding a task and started adding the alert dialogues

--- app/src/main/java/com/example/andrewmcclary/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/andrewmcclary/taskmanager/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.andrewmcclary.taskmanager;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.button_add_task)
+    protected void addTaskClicked(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("What is the title of your Task?");
+
+        builder.setMessage("When is your task due by?");
+
+        builder.setMessage("What are you doing in this task?");
+        //creating alert dialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
